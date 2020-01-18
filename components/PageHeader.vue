57e04ac149b3712f682fadd3a1f57678958cb9bf
Arne Foth <arnefoth@arne.local>
2020-01-19 00:10:58 +0100

    Erster Commit

---
+++ components/PageHeader.vue
@@ -0,0 +1,9 @@
+<template>
+  <header class="text-center">
+    <a class="text-gray-800 text-3xl font-bold" href="/">
+      <h1>
+        Static Blog
+      </h1>
+    </a>
+  </header>
+</template>
